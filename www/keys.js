7f68f01531f29c1557210ceaa595c0a37352ebb7
k18rs124 <k18rs124@st.kyusan-u.ac.jp>
2021-01-06 12:28:33 +0900

    ボタン遷移ページの追加

---
+++ www/keys.js
@@ -0,0 +1,4 @@
+// This is a JavaScript file
+
+var appKey="bf0865acaa4ed97c48ef06701ccb22f647199d266c6eabea8c38af6cb75ddd18";
+var clientKey="34a86cb9325079916eaf986fe5f5e52a24521b31cd127ca04f852a7c74350bea";
